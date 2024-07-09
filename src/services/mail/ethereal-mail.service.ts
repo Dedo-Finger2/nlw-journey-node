1eada6a6fb8ff1d9e6d35c2adcbb68447bd24ec5
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-09 20:44:49 -0300

    feat: add ethereal-mail-service

---
+++ src/services/mail/ethereal-mail.service.ts
@@ -0,0 +1,35 @@
+import { MailMessage, MailService } from "./mail.service";
+import nodemailer, { TestAccount, Transporter } from "nodemailer";
+
+export class EtherealMailService implements MailService {
+  private constructor(
+    private readonly transporter: Transporter,
+    private readonly account: TestAccount
+  ) {}
+
+  private static async build() {
+    const account = await nodemailer.createTestAccount();
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: account.user,
+        pass: account.pass
+      }
+    });
+
+    return new EtherealMailService(transporter, account);
+  }
+
+  async sendMail(message: MailMessage): Promise<void> {
+    const mailService = await EtherealMailService.build();
+
+    await mailService.sendMail({
+      to: message.to,
+      from: message.from,
+      subject: message.subject,
+      html: message.html
+    });
+  }
+}
