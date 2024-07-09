a88deec5587be3deb01a9a813c0030344cfd8e0d
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-09 08:19:45 -0300

    feat: add fastify cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*"
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
