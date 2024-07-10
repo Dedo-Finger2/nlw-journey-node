28a8cb7485a0b7f4cdc008fd22c3bbb4814bd759
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-10 09:11:26 -0300

    feat: add baseResponse zod object

--- src/interfaces/http.type.ts
+++ src/interfaces/http.type.ts
@@ -1,13 +1,16 @@
-export enum HTTP_RESPONSE {
+import { z } from "zod";
+
+export enum HTTP_STATUS_CODE {
   BAD_REQUEST = 400,
   OK = 200,
   CREATED = 201,
   INTERNAL_SERVER_ERROR = 500
 }
 
-export type HttpResponse<Body> = {
-  code: number;
-  body: Body;
-  error: boolean;
-  success: boolean;
-};
+export const baseResponseSchema = z.object({
+  code: z.nativeEnum(HTTP_STATUS_CODE),
+  error: z.boolean(),
+  success: z.boolean()
+});
+
+export type BaseHttpResponse = z.infer<typeof baseResponseSchema>;
