dd5ec7eb788d7b74d6c253433d949bca54761240
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-10 09:12:03 -0300

    refactor: controller interface add success and error type generics

--- src/interfaces/controller.interface.ts
+++ src/interfaces/controller.interface.ts
@@ -1,14 +1,30 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-import { HttpResponse } from "./http.type";
+import {
+  FastifyReply,
+  FastifyRequest,
+  RawReplyDefaultExpression,
+  RawRequestDefaultExpression,
+  RawServerDefault,
+  RouteGenericInterface
+} from "fastify";
 
 export type ControllerRequest<ControllerRequestBody> = FastifyRequest<{
   Body: ControllerRequestBody;
 }>;
-export type ControllerReply = FastifyReply;
 
-export interface Controller<RequestBody, ResponseBody> {
+export interface ReplyPayload<Payload> extends RouteGenericInterface {
+  Reply: Payload;
+}
+
+export type ControllerReply<Payload> = FastifyReply<
+  RawServerDefault,
+  RawRequestDefaultExpression,
+  RawReplyDefaultExpression,
+  ReplyPayload<Payload>
+>;
+
+export interface Controller<RequestBody, SuccessResponseBody, ErrorResponseBody> {
   handle(
     request: ControllerRequest<RequestBody>,
-    reply: ControllerReply
-  ): Promise<HttpResponse<ResponseBody>>;
+    reply: ControllerReply<SuccessResponseBody | ErrorResponseBody>
+  ): Promise<SuccessResponseBody | ErrorResponseBody>;
 }
