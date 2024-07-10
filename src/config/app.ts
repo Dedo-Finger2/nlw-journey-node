db984e82a701fd2fa8f052d269af789bc804c3a1
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-10 20:52:53 -0300

    refactor: move app configuration to it's own file

---
+++ src/config/app.ts
@@ -0,0 +1,17 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { createTrip } from "./../routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*"
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+
+export { app };
