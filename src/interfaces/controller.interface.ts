cbaf44728be29349a02f21eb1fa800ba45282606
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-10 10:15:08 -0300

    refactor: controller no longer access request and reply

--- src/interfaces/controller.interface.ts
+++ src/interfaces/controller.interface.ts
@@ -1,30 +1,3 @@
-import {
-  FastifyReply,
-  FastifyRequest,
-  RawReplyDefaultExpression,
-  RawRequestDefaultExpression,
-  RawServerDefault,
-  RouteGenericInterface
-} from "fastify";
-
-export type ControllerRequest<ControllerRequestBody> = FastifyRequest<{
-  Body: ControllerRequestBody;
-}>;
-
-export interface ReplyPayload<Payload> extends RouteGenericInterface {
-  Reply: Payload;
-}
-
-export type ControllerReply<Payload> = FastifyReply<
-  RawServerDefault,
-  RawRequestDefaultExpression,
-  RawReplyDefaultExpression,
-  ReplyPayload<Payload>
->;
-
-export interface Controller<RequestBody, SuccessResponseBody, ErrorResponseBody> {
-  handle(
-    request: ControllerRequest<RequestBody>,
-    reply: ControllerReply<SuccessResponseBody | ErrorResponseBody>
-  ): Promise<SuccessResponseBody | ErrorResponseBody>;
+export interface Controller<RequestBody, ResponseBody> {
+  handle(input: RequestBody): Promise<ResponseBody>;
 }
--- src/trip/http/controllers/create-trip.controller.ts
+++ src/trip/http/controllers/create-trip.controller.ts
@@ -1,32 +1,28 @@
 import { AppService } from "../../../interfaces/app-service.interface";
-import { HTTP_STATUS_CODE } from "../../../interfaces/http.type";
-import { z } from "zod";
-import {
-  Controller,
-  ControllerReply,
-  ControllerRequest
-} from "../../../interfaces/controller.interface";
+import { Controller } from "../../../interfaces/controller.interface";
+import { BaseHttpResponse, HTTP_STATUS_CODE } from "../../../interfaces/http.type";
 import {
   CreateTripServiceRequest,
   CreateTripServiceResponse
 } from "../../app-services/create-trip.service";
-import {
-  createTripErrorResponseSchema,
-  createTripSchema,
-  createTripSuccessResponseSchema
-} from "../../schemas/create-trip.schema";
 
-type CreateTripControllerRequestBody = z.infer<typeof createTripSchema>;
-type CreateTripSuccessResponse = z.infer<typeof createTripSuccessResponseSchema>;
-type CreateTripErrorResponse = z.infer<typeof createTripErrorResponseSchema>;
+type CreateTripControllerRequestBody = {
+  destination: string;
+  starts_at: Date;
+  ends_at: Date;
+  owner_name: string;
+  owner_email: string;
+  emails_to_invite: string[];
+};
+
+type CreateTripControllerResponse = BaseHttpResponse & {
+  body: {
+    tripId: string;
+  };
+};
 
 export class CreateTripController
-  implements
-    Controller<
-      CreateTripControllerRequestBody,
-      CreateTripSuccessResponse,
-      CreateTripErrorResponse
-    >
+  implements Controller<CreateTripControllerRequestBody, CreateTripControllerResponse>
 {
   constructor(
     private readonly createTripService: AppService<
@@ -36,36 +32,26 @@ export class CreateTripController
   ) {}
 
   async handle(
-    request: ControllerRequest<CreateTripControllerRequestBody>,
-    reply: ControllerReply<CreateTripSuccessResponse | CreateTripErrorResponse>
-  ): Promise<CreateTripSuccessResponse | CreateTripErrorResponse> {
+    input: CreateTripControllerRequestBody
+  ): Promise<CreateTripControllerResponse> {
     const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } =
-      request.body;
+      input;
 
-    try {
-      const { tripId } = await this.createTripService.execute({
-        destination,
-        emailsToInvite: emails_to_invite,
-        startsAt: starts_at,
-        endsAt: ends_at,
-        isConfirmed: true,
-        ownerName: owner_name,
-        ownerEmail: owner_email
-      });
+    const { tripId } = await this.createTripService.execute({
+      destination,
+      emailsToInvite: emails_to_invite,
+      startsAt: starts_at,
+      endsAt: ends_at,
+      isConfirmed: true,
+      ownerName: owner_name,
+      ownerEmail: owner_email
+    });
 
-      return reply.status(HTTP_STATUS_CODE.CREATED).send({
-        code: HTTP_STATUS_CODE.CREATED,
-        error: false,
-        success: true,
-        body: { tripId }
-      });
-    } catch (error) {
-      return reply.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({
-        code: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
-        error: true,
-        success: false,
-        body: "Internal Server Error."
-      });
-    }
+    return {
+      code: HTTP_STATUS_CODE.CREATED,
+      error: false,
+      success: true,
+      body: { tripId }
+    };
   }
 }
