0b9a4724876107e5f34fe381869385e713185259
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-10 20:40:31 -0300

    feat: finish create-trip route

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,6 +1,5 @@
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
-import nodemailer from "nodemailer";
 import "dayjs/locale/pt-br";
 import type { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
@@ -8,15 +7,14 @@ import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { getMailClient } from "../lib/mail";
 
-
 dayjs.extend(localizedFormat);
 dayjs.locale("pt-br");
 
 // eslint-disable-next-line require-await
 export async function createTrip(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .post("/trips", {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
       schema: {
         body: z.object({
           destination: z.string().min(4),
@@ -27,8 +25,16 @@ export async function createTrip(app: FastifyInstance) {
           emails_to_invite: z.array(z.string().email())
         })
       }
-    }, async (request, reply) => {
-      const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body;
+    },
+    async (request, reply) => {
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_to_invite
+      } = request.body;
 
       if (dayjs(starts_at).isBefore(new Date())) {
         throw new Error("Invalid trip start date");
@@ -52,7 +58,7 @@ export async function createTrip(app: FastifyInstance) {
                   is_owner: true,
                   is_confirmed: true
                 },
-                ...emails_to_invite.map(email => {
+                ...emails_to_invite.map((email) => {
                   return { email };
                 })
               ]
@@ -68,7 +74,7 @@ export async function createTrip(app: FastifyInstance) {
 
       const mail = await getMailClient();
 
-      const message = await mail.sendMail({
+      await mail.sendMail({
         from: {
           name: "Equipe planner",
           address: "planner@company.com"
@@ -102,10 +108,7 @@ export async function createTrip(app: FastifyInstance) {
         `.trim()
       });
 
-      console.log(nodemailer.getTestMessageUrl(message));
-
-      return reply.status(201).send(
-        { tripId: trip.id }
-      );
-    });
+      return reply.status(201).send({ tripId: trip.id });
+    }
+  );
 }
