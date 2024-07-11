2894ad02d765c5a473c3ba12f53f2f7ec98a6e76
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-11 14:55:15 -0300

    feat: add swagger integration with fastify + zod

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,11 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
 import { createTrip } from "../routes/create-trip.route";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler
+} from "fastify-type-provider-zod";
 import { confirmTrip } from "../routes/confirm-trip.route";
 import { confirmParticipantOnTrip } from "../routes/confirm-participant.route";
 import { createActivity } from "../routes/create-activity.route";
@@ -15,10 +19,30 @@ import { getTripDetails } from "../routes/get-trip-details.route";
 import { getTripParticipant } from "../routes/get-participant.route";
 import { errorHandler } from "../lib/error-handler";
 
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
 const app = fastify();
 
 app.register(fastifyCors, {
   origin: "*"
+});
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "Plann.er",
+      description:
+        "Especificações da API para o back-end da aplicação plann.er feita durante o evento NLW Journey da Rocketseat.",
+      version: "1.0.0"
+    }
+  },
+  transform: jsonSchemaTransform
+});
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs"
 });
 
 app.setValidatorCompiler(validatorCompiler);
