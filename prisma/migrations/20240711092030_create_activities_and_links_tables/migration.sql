fa113fb2d1bfb40d9aea7ec756c8c992f150d8b1
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-11 06:20:45 -0300

    feat: add activities and links table

---
+++ prisma/migrations/20240711092030_create_activities_and_links_tables/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "activivies" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "occurs_at" DATETIME NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "activivies_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "links" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "links_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
